48dd216a97484842687cae853cb6a960888b0229
Gaurav-0301 <gauravkakpure2006@gmail.com>
2026-03-19 22:37:24 +0530

    fix frontend error

--- Server/index.js
+++ Server/index.js
@@ -11,13 +11,12 @@ const cors = require("cors");
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://gatekeeper-sx2h.onrender.com"
+    "https://vpms-psi.vercel.app"  
   ],
   credentials: true
 }));
 
-// ❌ REMOVE THIS LINE
-// app.options('/*', cors());
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
