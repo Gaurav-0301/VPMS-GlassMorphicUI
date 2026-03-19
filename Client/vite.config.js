1171a64bacf6f4d9d05d3866b965997694044535
Gaurav-0301 <gauravkakpure2006@gmail.com>
2026-03-19 09:52:21 +0530

    add render yaml routing fix

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,25 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 
   build: {
-    chunkSizeWarningLimit: 1000, // optional (removes warning)
-
     rollupOptions: {
-      output: {
-        manualChunks: {
-          // React core
-          react: ['react', 'react-dom'],
-
-          // Router (if using)
-          router: ['react-router-dom'],
-
-          // Common libs (edit based on your project)
-          vendor: ['axios']
-        }
+      input: {
+        main: resolve(__dirname, 'index.html'),
       }
     }
   }
